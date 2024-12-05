a50534668f3e711bafc3a5430b973bf67e97c79b
luke hoogenboom <6035070@mborijnland.nl>
2024-12-05 11:52:02 +0100

    test

--- js/index.js
+++
@@ -1,6 +0,0 @@
-import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
-
-let options = { homeLink: 'https://lukeplays33.github.io/Pichai/index.html' }
-
-let pichai = new PichaiUX(options);
-pichai.initialize();
